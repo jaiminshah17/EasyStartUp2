73d300b7d141e7867f57f44f9a95c4e64f2f62f2
Shan Zhao <zhaoshan0613@outlook.com>
2018-10-18 18:01:50 -0400

    SignUp and Login pages for Enterprise

---
+++ app/src/main/java/com/example/jaiminshah/easystartup/SignUpPageforEnterprise.java
@@ -0,0 +1,13 @@
+package com.example.jaiminshah.easystartup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignUpPageforEnterprise extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up_pagefor_enterprise);
+    }
+}
