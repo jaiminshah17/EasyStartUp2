dcdaffe9ebae8cd9e7295ac1cdea7260040cfd54
Jaimin shah <jamesshah19996@gmail.com>
2018-10-20 20:10:54 -0400

    create sign up page

--- app/src/main/java/com/example/jaiminshah/easystartup/MainActivity.java
+++ app/src/main/java/com/example/jaiminshah/easystartup/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.jaiminshah.easystartup;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+     private Button button;
+     private Button button1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button =(Button)findViewById(R.id.x);
+        button1=(Button)findViewById(R.id.y);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v==button)
+        {
+            startActivity(new Intent(this,SignUpPageforEnterprise.class));
+        }
+
     }
 }
--- app/src/main/java/com/example/jaiminshah/easystartup/SignUpPageforEnterprise.java
+++ app/src/main/java/com/example/jaiminshah/easystartup/SignUpPageforEnterprise.java
@@ -1,13 +1,96 @@
 package com.example.jaiminshah.easystartup;
 
+import android.app.ProgressDialog;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class SignUpPageforEnterprise extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.FirebaseUser;
+
+
+public class SignUpPageforEnterprise extends AppCompatActivity implements View.OnClickListener {
+
+    private Button buttonregister;
+    private EditText editetxtemail;
+    private EditText edittextpassword;
+    private Button textviewsignup;
+    //private ProgressDialog progressDialog;
+    private FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up_pagefor_enterprise);
+        buttonregister = (Button) findViewById(R.id.email_sign_in_button);
+        editetxtemail = (EditText) findViewById(R.id.email);
+        edittextpassword = (EditText) findViewById(R.id.password);
+        textviewsignup = (Button) findViewById(R.id.email_alreadysign_in_button);
+        buttonregister.setOnClickListener(this);
+        textviewsignup.setOnClickListener(this);
+        //progressDialog = new ProgressDialog(this);
+        mAuth = FirebaseAuth.getInstance();
+
     }
+
+    private void registeruser() {
+        String email = editetxtemail.getText().toString().trim();
+        String password = edittextpassword.getText().toString().trim();
+
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(this, "please enter emaiil address", Toast.LENGTH_LONG).show();
+            //email is empty
+            return;
+        }
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(this, "please enter password", Toast.LENGTH_LONG).show();
+            //password is empty
+            return;
+        }
+        //progressDialog.setMessage("Registering there....");
+        //progressDialog.show();
+        mAuth.createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful())
+                        {
+                            Toast.makeText(SignUpPageforEnterprise.this,"Registration successfull",Toast.LENGTH_LONG).show();
+                        }
+                        else
+                        {
+                            Toast.makeText(SignUpPageforEnterprise.this,"Registration unsuccessfull",Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == buttonregister) {
+            registeruser();
+        }
+        if (v == textviewsignup) {
+            //will open signin activity
+        }
+    }
+
+
+
+
 }
+
