fda8aa5c75dbe16bdc0db24b7919734963309d66
bairenzhu <bz90924n@pace.edu>
2019-02-26 18:43:37 -0500

    UserProfile added

---
+++ app/src/main/java/com/example/jaiminshah/easystartup/UserProfile.java
@@ -0,0 +1,48 @@
+package com.example.jaiminshah.easystartup;
+
+public class UserProfile {
+    private String firstname;
+    private String lastname;
+    private String college;
+    private String userID;
+
+    public UserProfile(){}
+
+    public UserProfile(String firstname, String lastname, String college, String userID) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.college = college;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getCollege() {
+        return college;
+    }
+
+    public void setCollege(String college) {
+        this.college = college;
+    }
+}
